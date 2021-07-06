39fe0e27432111631cb21972c1c73162ce11bfbc
Marcos <MarcosAvila@protonmail.com>
2021-07-05 21:49:19 -0400

    ignore these

--- test_1/recipe_app.rb
+++ test_1/recipe_app.rb
@@ -76,6 +76,7 @@ end
 
 # View the form to add a new recipe
 get "/recipes/new" do
+  # @recipe_book
   erb :new_recipe, layout: :layout
 end
 
@@ -85,8 +86,7 @@ end
 #   @recipe_id = params[:recipe_id].to_i
 #   @recipe = @recipe_book.recipes.select { |recipe| recipe.id == @recipe_id }.first
 #   erb :view_recipe
-# end
-
+# endkz
 post "/recipes" do
   @recipe_book = session[:recipe_book]
   ingredients = make_arr(:ingredient)
