1af18b1564edb20d0af071526851a96647765fe9
Ainaa Azman <ainaasakinah@gmail.com>
2021-07-06 12:27:51 +0900

    change load recipe and save recipe methods

--- test_1/data/step.rb
+++ test_1/data/step.rb
@@ -1,8 +1,9 @@
 class Step
-  attr_accessor :id, :name
+  attr_accessor :id, :name, :origin_recipe_id
 
-  def initialize(id, name)
+  def initialize(id, name, origin_recipe_id)
     @id = id
     @name = name
+    @origin_recipe_id = origin_recipe_id
   end
 end
