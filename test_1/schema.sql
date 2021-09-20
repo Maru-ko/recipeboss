fd9e9b1e340401771c867744bfa619e2d7dd8dbe
Ainaa Azman <ainaasakinah@gmail.com>
2021-09-20 19:13:15 +0900

    writing rake comments for creating databse creation, schema upload, seeding and deleting databse on dev env

--- test_1/schema.sql
+++ test_1/schema.sql
@@ -1,20 +1,20 @@
--- CREATE TABLE recipes (
---   id serial PRIMARY KEY, 
---   name text NOT NULL, 
---   cook_time text
--- );
+CREATE TABLE recipes (
+  id serial PRIMARY KEY,
+  name text NOT NULL,
+  cook_time text
+);
 
--- CREATE TABLE ingredients (
---   id serial PRIMARY KEY, 
---   name text, 
---   recipe_id integer NOT NULL REFERENCES recipes (id) ON DELETE CASCADE
--- );
+CREATE TABLE ingredients (
+  id serial PRIMARY KEY,
+  name text,
+  recipe_id integer NOT NULL REFERENCES recipes (id) ON DELETE CASCADE
+);
 
--- CREATE TABLE steps (
---   id serial PRIMARY KEY, 
---   name text, 
---   recipe_id integer NOT NULL REFERENCES recipes (id) ON DELETE CASCADE
--- );
+CREATE TABLE steps (
+  id serial PRIMARY KEY,
+  name text,
+  recipe_id integer NOT NULL REFERENCES recipes (id) ON DELETE CASCADE
+);
 
 CREATE TABLE filters (
   id serial PRIMARY KEY,
@@ -27,15 +27,9 @@ CREATE TABLE filters_recipes (
   recipe_id integer NOT NULL REFERENCES recipes (id) ON DELETE CASCADE
 );
 
-INSERT INTO filters (name)
-VALUES('Vegetarian'), ('Gluten Free'), ('Vegan'), ('Pescatarian');
-
-INSERT INTO filters_recipes (filter_id, recipe_id)
-VALUES(1, 1), (1, 2), (2, 2), (1, 4), (2, 4);
-
 -- INSERT INTO recipes (name, cook_time) VALUES('pasta', '30 mins');
--- INSERT INTO ingredients (name, recipe_id) VALUES ('olive oil', 1); 
--- INSERT INTO ingredients (name, recipe_id) VALUES ('garlic', 1); 
+-- INSERT INTO ingredients (name, recipe_id) VALUES ('olive oil', 1);
+-- INSERT INTO ingredients (name, recipe_id) VALUES ('garlic', 1);
 -- INSERT INTO ingredients (name, recipe_id) VALUES ('onion', 1);
 
 -- INSERT INTO steps (name, recipe_id) VALUES ('do this thing 1', 1);
@@ -44,20 +38,25 @@ VALUES(1, 1), (1, 2), (2, 2), (1, 4), (2, 4);
 
 
 -- INSERT INTO recipes (name, cook_time) VALUES('salad', '30 mins');
--- INSERT INTO ingredients (name, recipe_id) VALUES ('olive oil', 2); 
--- INSERT INTO ingredients (name, recipe_id) VALUES ('garlic', 2); 
--- INSERT INTO ingredients (name, recipe_id) VALUES ('onion', 2); 
+-- INSERT INTO ingredients (name, recipe_id) VALUES ('olive oil', 2);
+-- INSERT INTO ingredients (name, recipe_id) VALUES ('garlic', 2);
+-- INSERT INTO ingredients (name, recipe_id) VALUES ('onion', 2);
 
 -- INSERT INTO steps (name, recipe_id) VALUES ('do this thing 1', 2);
 -- INSERT INTO steps (name, recipe_id) VALUES ('do this thing 2', 2);
 -- INSERT INTO steps (name, recipe_id) VALUES ('do this thing 3', 2);
 
 -- INSERT INTO recipes (name, cook_time) VALUES('soup', '30 mins');
--- INSERT INTO ingredients (name, recipe_id) VALUES ('olive oil', 3); 
--- INSERT INTO ingredients (name, recipe_id) VALUES ('garlic', 3); 
--- INSERT INTO ingredients (name, recipe_id) VALUES ('onion', 3); 
+-- INSERT INTO ingredients (name, recipe_id) VALUES ('olive oil', 3);
+-- INSERT INTO ingredients (name, recipe_id) VALUES ('garlic', 3);
+-- INSERT INTO ingredients (name, recipe_id) VALUES ('onion', 3);
 
 -- INSERT INTO steps (name, recipe_id) VALUES ('do this thing 1', 3);
 -- INSERT INTO steps (name, recipe_id) VALUES ('do this thing 2', 3);
 -- INSERT INTO steps (name, recipe_id) VALUES ('do this thing 3',3);
 
+-- INSERT INTO filters (name)
+-- VALUES('Vegetarian'), ('Gluten Free'), ('Vegan'), ('Pescatarian');
+
+-- INSERT INTO filters_recipes (filter_id, recipe_id)
+-- VALUES(1, 1), (1, 2), (2, 2), (1, 4), (2, 4);
